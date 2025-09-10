815cc22251d744ab1c73c02f98f0c17c6fe1311e
Theia Vogel <theia@vgel.me>
2025-09-09 21:26:32 -0700

    fix env name

--- train.py
+++ train.py
@@ -6,6 +6,14 @@ import verifiers as vf
 """
 # Multi-GPU training (single node, 4 training + 4 inference)
 
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source ~/.local/bin/env
+uv pip install --system verifiers vllm fastapi uvloop wandb deepspeed
+uv pip install --system --no-build-isolation flash-attn==2.8.2 git+https://github.com/Dao-AILab/flash-attention@v2.8.2#subdirectory=csrc/fused_dense_lib
+uv pip install --system -e .
+[ -d /workspace ] && export HF_HOME=/workspace/hf
+wandb login
+
 CUDA_VISIBLE_DEVICES=4,5,6,7 vf-vllm --model 'Qwen/Qwen2.5-7B-Instruct' --tensor_parallel_size 4 --max_model_len 4096 \
     --dtype bfloat16 --gpu_memory_utilization 0.9 --enable_prefix_caching True --host 0.0.0.0 --port 8000
 
@@ -17,7 +25,7 @@ def main(args):
     env_args = json.loads(args.env_args)
 
     model, tokenizer = vf.get_model_and_tokenizer(model_name)
-    vf_env = vf.load_environment(env_id="vf-simple-reward-hack", **env_args)
+    vf_env = vf.load_environment(env_id="vf-simple-reward-hacking", **env_args)
     
     run_name = "simple-reward-hack_" + model_name.split("/")[-1].lower()
 
@@ -44,7 +52,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", "-m", type=str, default="Qwen/Qwen2.5-7B-Instruct")
-    parser.add_argument("--env-args", "-a", type=str, default="{}")
+    parser.add_argument("--model", "-m", default="Qwen/Qwen2.5-7B-Instruct")
+    parser.add_argument("--env-args", "-a", default="{}")
     args = parser.parse_args()
     main(args)
