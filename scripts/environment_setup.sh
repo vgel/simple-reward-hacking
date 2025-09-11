763dede9e007d246d38859349195f5730db895b9
Theia Vogel <theia@vgel.me>
2025-09-11 14:14:57 -0700

    crimes

--- scripts/environment_setup.sh
+++ scripts/environment_setup.sh
@@ -1,5 +1,7 @@
 # Known working versions for this instance
 uv venv --python python3.12
-uv pip install 'verifiers[all]==0.1.3.post0' 'vllm==0.8.5.post1' fastapi uvloop wandb deepspeed accelerate
+uv pip install 'verifiers[all]==0.1.3.post0'
+# this is illegal (verifiers wants vllm>=0.9.2) so do it after
+uv pip 'vllm==0.8.5.post1' fastapi uvloop wandb deepspeed accelerate
 uv pip install 'flash-attn==2.6.1' --no-build-isolation
 uv pip install -e .
